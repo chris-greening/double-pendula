3de9a93bcbcec9f6c397369ec2de323ec291a83c
Chris <chris@christophergreening.com>
2022-10-29 15:35:34 -0400

    FIX: added missing m1 and m2 sets for create_multiple_double_pendula

--- double_pendulum.py
+++ double_pendulum.py
@@ -87,6 +87,8 @@ class DoublePendulum:
             double_pendulum = cls(
                 L1=L1,
                 L2=L2,
+                m1=m1, 
+                m2=m2
                 y0=[initial_theta, 0, -10, 0]
             )
             pendula.append(double_pendulum)
