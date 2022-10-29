40952b63174ebf2c02888276987833bb5955d63f
Chris <chris@christophergreening.com>
2022-10-29 15:23:46 -0400

    DELETE: removed z1dot hardcode from _derivative

--- double_pendulum.py
+++ double_pendulum.py
@@ -140,7 +140,6 @@ def _derivative(y, t, L1, L2, m1, m2, g):
     c, s = np.cos(theta1-theta2), np.sin(theta1-theta2)
 
     z1dot = _calculate_z1dot(m1, m2, L1, L2, theta1, theta2, z1, z2, g, s, c)
-    z1dot = (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
     z2dot = ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
     return theta1dot, z1dot, theta2dot, z2dot
 
