6ad5c5712cdcda8588a9cef66acbdaee2f2214a1
Chris <chris@christophergreening.com>
2022-10-29 15:23:19 -0400

    REFACTOR: used _calculate_z1dot to calculate the indiividual z1dot component

--- double_pendulum.py
+++ double_pendulum.py
@@ -99,7 +99,7 @@ class DoublePendulum:
         """Solve the ODE and calculate the path for both pendulum's in the 
         system"""
         self.y = odeint(
-            self._derivative, 
+            _derivative, 
             self.y0, 
             self.t, 
             args=(
@@ -127,24 +127,23 @@ class DoublePendulum:
             columns=["theta1", "dtheta1", "theta2", "dtheta2"]
         )
 
-    @staticmethod
-    def _derivative(y, t, L1, L2, m1, m2, g):
-        """Return the first derivatives of y = theta1, z1, theta2, z2."""
-        
-        # Unpack initial conditions
-        theta1, z1, theta2, z2 = y
-        theta1dot, theta2dot = z1, z2
-
-        c, s = np.cos(theta1-theta2), np.sin(theta1-theta2)
-
-        z1dot = (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
-        z2dot = ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
-        return theta1dot, z1dot, theta2dot, z2dot
-
-    @staticmethod
-    def _calculate_z1dot(m1, m2, L1, L2, theta1, theta2, z1, z2, g, s, c) -> float:
-        """Return calculated z1dot"""
-        return (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
-
     def __repr__(self):
         return f"< DoublePendulum: L1={self.pendulum1.L} m1={self.pendulum1.m} L2={self.pendulum2.L} m2={self.pendulum2.m} y0={self.y0} >"
+
+def _derivative(y, t, L1, L2, m1, m2, g):
+    """Return the first derivatives of y = theta1, z1, theta2, z2."""
+    
+    # Unpack initial conditions
+    theta1, z1, theta2, z2 = y
+    theta1dot, theta2dot = z1, z2
+
+    c, s = np.cos(theta1-theta2), np.sin(theta1-theta2)
+
+    z1dot = _calculate_z1dot(m1, m2, L1, L2, theta1, theta2, z1, z2, g, s, c)
+    z1dot = (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
+    z2dot = ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
+    return theta1dot, z1dot, theta2dot, z2dot
+
+def _calculate_z1dot(m1, m2, L1, L2, theta1, theta2, z1, z2, g, s, c) -> float:
+    """Return calculated z1dot"""
+    return (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
