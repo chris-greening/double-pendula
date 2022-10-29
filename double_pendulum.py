a8ea6464ec3017843f0daff7da2e81ca7c4cab05
Chris <chris@christophergreening.com>
2022-10-29 14:51:04 -0400

    REFACTOR: changed indentation of self.y in _calculate_system

--- double_pendulum.py
+++ double_pendulum.py
@@ -98,10 +98,15 @@ class DoublePendulum:
     def _calculate_system(self, L1: int, m1: int, L2: int, m2: int) -> None:
         """Solve the ODE and calculate the path for both pendulum's in the 
         system"""
-        self.y = odeint(self._derivative, self.y0, self.t, 
-                        args=(self.pendulum1.L, self.pendulum2.L, 
-                              self.pendulum1.m, self.pendulum2.m,
-                              self.g)
+        self.y = odeint(
+            self._derivative, 
+            self.y0, 
+            self.t, 
+            args=(
+                self.pendulum1.L, self.pendulum2.L, 
+                self.pendulum1.m, self.pendulum2.m,
+                self.g
+            )
         )
 
         # Calculate individual pendulum paths
