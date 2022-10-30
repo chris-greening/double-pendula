e01591f628504e0ff8c288bff0a6a8cc96630531
Chris <chris@christophergreening.com>
2022-10-30 12:44:22 -0400

    DOCS: added docstring to _calculate_z2dot

--- double_pendula/equations.py
+++ double_pendula/equations.py
@@ -31,4 +31,5 @@ def _calculate_z1dot(m1, m2, L1, L2, theta1, theta2, z1, z2, g, s, c) -> float:
     return (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
 
 def _calculate_z2dot(m1, m2, L1, L2, theta1, theta2, z1, z2, g, s, c) -> float:
+    """Return calculated z2dot"""
     return ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
