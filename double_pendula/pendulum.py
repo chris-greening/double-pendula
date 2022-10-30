65e329611c389cafa33b0ea99e62903ed4516843
Chris <chris@christophergreening.com>
2022-10-30 14:24:14 -0400

    REMOVE: removed trailing whitespace

--- double_pendula/pendulum.py
+++ double_pendula/pendulum.py
@@ -9,9 +9,9 @@ import pandas as pd
 
 class Pendulum:
     def __init__(self, L: float = 1.0, m: float = 1.0) -> None:
-        """A single pendulum with rod of length L and a bob of mass M fixed at 
+        """A single pendulum with rod of length L and a bob of mass M fixed at
         the end
-        
+
         Parameters
         ----------
         L : float
@@ -24,27 +24,27 @@ class Pendulum:
 
     def calculate_path(self, theta: float, dtheta: float, x0: float = 0, y0: float = 0) -> None:
         """Calculate the (x,y) coordinate path of the pendulum
-        
+
         Parameters
         ----------
-        theta : float 
+        theta : float
             Angle of the single pendulum rod
-        dtheta : float 
+        dtheta : float
             Change of angle of the pendulum rod
-        x0 : float 
-            x-offset. If this is only a single pendulum this is zero but if 
-            this is a pendulum fixed to the bob of another pendulum then this 
+        x0 : float
+            x-offset. If this is only a single pendulum this is zero but if
+            this is a pendulum fixed to the bob of another pendulum then this
             is nonzero
-        y0 : float 
-            y-offset. If this is only a single pendulum this is zero but if 
-            this is a pendulum fixed to the bob of another pendulum then this 
+        y0 : float
+            y-offset. If this is only a single pendulum this is zero but if
+            this is a pendulum fixed to the bob of another pendulum then this
             is nonzero
         """
         self.theta = theta
         self.dtheta = dtheta
         self.x = self.L*np.sin(self.theta) + x0
         self.y = self.L*np.cos(self.theta) + y0
-        self.df = pd.DataFrame({"theta": self.theta, "dtheta": self.dtheta, 
+        self.df = pd.DataFrame({"theta": self.theta, "dtheta": self.dtheta,
                                 "x": self.x, "y": self.y})
 
     def get_max_x(self) -> float:
